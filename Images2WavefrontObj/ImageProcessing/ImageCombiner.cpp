6672dda34a1dfabc1be1cab73c625d3fa963b580
maesi <marcelfuchs@gmx.ch>
2018-09-02 20:11:19 +0200

    Implementing comparing gradient images (not working yet)

--- Images2WavefrontObj/ImageProcessing/ImageCombiner.cpp
+++ Images2WavefrontObj/ImageProcessing/ImageCombiner.cpp
@@ -47,14 +47,26 @@ bool ImageCombiner::CombineTwoImages(QImage& p_finalImage, const QImage& p_exten
 {
     bool successful = true;
 
-    int observeMatrix[KernelSize][KernelSize];
-    QSize imageSize = QSize(p_finalImage.width(), p_finalImage.height());
-    QImage gradientImageX(imageSize, QImage::Format_RGB32);
-    QImage gradientImageY(imageSize, QImage::Format_RGB32);
+    QSize finalImageSize = QSize(p_finalImage.width(), p_finalImage.height());
+    QImage gradientFinalImage(finalImageSize, QImage::Format_RGB32);
+    GetGradientImage(p_finalImage, gradientFinalImage);
+    
+    QSize extensionImageSize = QSize(p_extensionImage.width(), p_extensionImage.height());
+    QImage gradientExtensionImage(extensionImageSize, QImage::Format_RGB32);
+    GetGradientImage(p_extensionImage, gradientExtensionImage);
+    AnalyzeLeftSide(p_finalImage, p_extensionImage, gradientFinalImage, gradientExtensionImage);
+    
+    p_finalImage = gradientFinalImage;
 
-    for (int x = KernelNeighbour; x < (p_finalImage.width() - KernelNeighbour - 1); x++)
+    return successful;
+}
+
+void ImageCombiner::GetGradientImage(const QImage& p_Image, QImage& p_gradientImage)
+{
+    int observeMatrix[KernelSize][KernelSize];
+    for (int x = KernelNeighbour; x < (p_Image.width() - KernelNeighbour - 1); x++)
     {
-        for (int y = KernelNeighbour; y < (p_finalImage.height() - KernelNeighbour -1); y++)
+        for (int y = KernelNeighbour; y < (p_Image.height() - KernelNeighbour - 1); y++)
         {
             for (int kernelX = -KernelNeighbour; kernelX <= KernelNeighbour; kernelX++)
             {
@@ -62,27 +74,22 @@ bool ImageCombiner::CombineTwoImages(QImage& p_finalImage, const QImage& p_exten
                 {
                     int indexX = kernelX + KernelNeighbour;
                     int indexY = kernelY + KernelNeighbour;
-                    observeMatrix[indexX][indexY] = qGray(p_finalImage.pixel(x + indexX, y + indexY));
+                    observeMatrix[indexX][indexY] = qGray(p_Image.pixel(x + indexX, y + indexY));
                 }
             }
-            UpdateGradientImage(gradientImageX, gradientImageY, observeMatrix, x, y);
+            UpdateGradientImage(p_gradientImage, observeMatrix, x, y);
         }
     }
-    
-    p_finalImage = gradientImageX;
-
-    return successful;
 }
 
-void ImageCombiner::UpdateGradientImage(QImage& p_gradientImageX, QImage& p_gradientImageY, int p_ObserveMatrix[KernelSize][KernelSize], int p_ImageX, int p_ImageY)
+void ImageCombiner::UpdateGradientImage(QImage& p_gradientImage, int p_ObserveMatrix[KernelSize][KernelSize], int p_ImageX, int p_ImageY)
 {
     int convoluteX = ConvoluteMatrices(p_ObserveMatrix, SobelKernelMatrixX);
     int convoluteY = ConvoluteMatrices(p_ObserveMatrix, SobelKernelMatrixY);
     
     double sum = sqrt(pow(static_cast<double>(convoluteX), 2) + pow(static_cast<double>(convoluteY), 2));
 
-    p_gradientImageX.setPixel(p_ImageX, p_ImageY, qRgb(sum, sum, sum));
-    p_gradientImageY.setPixel(p_ImageX, p_ImageY, qRgb(convoluteY, convoluteY, convoluteY));
+    p_gradientImage.setPixel(p_ImageX, p_ImageY, qRgb(sum, sum, sum));
 }
 
 int ImageCombiner::ConvoluteMatrices(const int p_ObserveMatrix[KernelSize][KernelSize], const int p_KernelMatrix[KernelSize][KernelSize])
@@ -105,4 +112,102 @@ int ImageCombiner::ConvoluteMatrices(const int p_ObserveMatrix[KernelSize][Kerne
         sum = 0;
     }
     return sum;
+}
+
+void ImageCombiner::AnalyzeLeftSide(QImage& p_finalImage, const QImage& p_extensionImage, const QImage& p_gradientFinalImage, const QImage& p_gradientExtensionImage)
+{
+    for (int y = 0; (y + OverlapPixels) < p_gradientFinalImage.height(); y += OverlapPixels)
+    {
+        bool overlapFound = FindOverlap(p_gradientFinalImage, p_gradientExtensionImage, 0, y);
+
+        if (overlapFound)
+        {
+            break;
+        }
+    }
+
+}
+
+bool ImageCombiner::FindOverlap(const QImage& p_gradientFinalImage, const QImage& p_gradientExtensionImage, int p_startX, int p_startY)
+{
+    bool overlapFound = false;
+
+    QRgb xExpectedPixels[OverlapPixels] = {};
+    QRgb yExpectedPixels[OverlapPixels] = {};
+    bool xUpdated = false;
+
+    for (int xFinalImageIndex = p_startX; xFinalImageIndex < p_startX + OverlapPixels; xFinalImageIndex++)
+    {
+        for (int yFinalImageIndex = p_startY; yFinalImageIndex < p_startY + OverlapPixels; yFinalImageIndex++)
+        {
+            QRgb finalImagePixel = p_gradientFinalImage.pixel(xFinalImageIndex, yFinalImageIndex);
+         
+            if (!xUpdated)
+            {
+                xExpectedPixels[xFinalImageIndex - p_startX] = finalImagePixel;
+                xUpdated = true;
+            }
+            yExpectedPixels[yFinalImageIndex - p_startY] = finalImagePixel;
+        }
+        xUpdated = false;
+    }
+
+    overlapFound = FindOverlapPixels(p_gradientExtensionImage, xExpectedPixels, yExpectedPixels);
+
+    return overlapFound;
+}
+
+bool ImageCombiner::FindOverlapPixels(const QImage& p_image, QRgb* p_xExpectedPixels, QRgb* p_yExpectedPixels)
+{
+    bool overlapFound = false;
+
+    int y = 0;
+    for (int x = 0; x < p_image.width() - OverlapPixels; x++)
+    {
+        for (; y < p_image.height() - OverlapPixels; y++)
+        {
+            int yExpectedPixelIndex = 0;
+            do {
+                QRgb imagePixel = p_image.pixel(x, y + yExpectedPixelIndex);
+                overlapFound = HasSameGrayValue(imagePixel, p_yExpectedPixels[yExpectedPixelIndex]);
+                yExpectedPixelIndex++;
+            } while (overlapFound || yExpectedPixelIndex < OverlapPixels);
+            
+            if (overlapFound)
+            {
+                break;
+            }
+        }
+
+        if (overlapFound)
+        {
+            int xExpectedPixelIndex = 0;
+            do {
+                QRgb imagePixel = p_image.pixel(x + xExpectedPixelIndex, y);
+                overlapFound = HasSameGrayValue(imagePixel, p_xExpectedPixels[xExpectedPixelIndex]);
+                xExpectedPixelIndex++;
+            } while (overlapFound || xExpectedPixelIndex < OverlapPixels);
+
+            if (overlapFound)
+            {
+                m_overlapXStart = x;
+                m_overlapYStart = y;
+                break;
+            }
+        }
+    }
+
+    return overlapFound;
+}
+
+bool ImageCombiner::HasSameGrayValue(QRgb p_firstValue, QRgb p_secondValue)
+{
+    bool hasSameGrayValue = false;
+
+    if (p_firstValue > (p_secondValue - AllowedGrayScaleDifference) && p_firstValue < (p_secondValue + AllowedGrayScaleDifference))
+    {
+        hasSameGrayValue = true;
+    }
+
+    return hasSameGrayValue;
 }
--- Images2WavefrontObj/ImageProcessing/ImageCombiner.h
+++ Images2WavefrontObj/ImageProcessing/ImageCombiner.h
@@ -16,12 +16,24 @@ public:
 private:
     static constexpr int KernelNeighbour = 1;
     static constexpr int KernelSize = 3;
+    static constexpr int OverlapPixels = 20;
+    static constexpr int AllowedGrayScaleDifference = 20;
 
     static constexpr int SobelKernelMatrixX[KernelSize][KernelSize] = { {1,0,-1},{2,0,-2},{1,0,-1 } };
     static constexpr int SobelKernelMatrixY[KernelSize][KernelSize] = { {1,2,1},{0,0,0},{-1,-2,-1} };
 
+    int m_overlapXStart{ 0 };
+    int m_overlapYStart{ 0 };
+
     bool CombineTwoImages(QImage& p_finalImage, const QImage& p_extensionImage);
-    void UpdateGradientImage(QImage& p_gradientImageX, QImage& p_gradientImageY, int p_ObserveMatrix[KernelSize][KernelSize], int p_ImageX, int p_ImageY);
+    void GetGradientImage(const QImage& p_Image, QImage& p_gradientImage);
+    void UpdateGradientImage(QImage& p_gradientImage, int p_ObserveMatrix[KernelSize][KernelSize], int p_ImageX, int p_ImageY);
     int ConvoluteMatrices(const int p_ObserveMatrix[KernelSize][KernelSize], const int p_KernelMatrix[KernelSize][KernelSize]);
+
+    void AnalyzeLeftSide(QImage& p_finalImage, const QImage& p_extensionImage, const QImage& p_gradientFinalImage, const QImage& p_gradientExtensionImage);
+
+    bool FindOverlap(const QImage& p_gradientFinalImage, const QImage& p_gradientExtensionImage, int p_startX, int p_startY);
+    bool FindOverlapPixels(const QImage& p_image, QRgb* p_xExpectedPixels, QRgb* p_yExpectedPixels);
+    bool HasSameGrayValue(QRgb p_firstValue, QRgb p_secondValue);
 };
 
