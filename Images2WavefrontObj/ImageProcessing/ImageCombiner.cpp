aaa29af1ae418ec6c07455fe24c4485f44abf813
maesi <marcelfuchs@gmx.ch>
2018-11-03 16:44:35 +0100

    Updated combining images

--- Images2WavefrontObj/ImageProcessing/ImageCombiner.cpp
+++ Images2WavefrontObj/ImageProcessing/ImageCombiner.cpp
@@ -11,6 +11,11 @@ QImage ImageCombiner::CombineImages(const QStringList& p_images)
     QImage finalImage;
     bool successful = true;
     bool firstImage = true;
+    m_overlapXStartPixelsFinalImage.clear();
+    m_overlapYStartPixelsFinalImage.clear();
+    m_overlapXStartPixelsExtensionImage.clear();
+    m_overlapYStartPixelsExtensionImage.clear();
+
     for (QString imageString : p_images)
     {
         QImageReader imageReader(imageString);
@@ -65,8 +70,11 @@ bool ImageCombiner::CombineTwoImages(QImage& p_finalImage, const QImage& p_exten
     QImage gradientExtensionImage(extensionImageSize, QImage::Format_RGB32);
     GetGradientImage(p_extensionImage, gradientExtensionImage);
     bool overlapFound = FindOverlap(p_finalImage, p_extensionImage);
-    
-    //p_finalImage = gradientFinalImage;
+    int correctOverlapIndex = FindCorrectOneOfOverlapCandidates(p_finalImage, p_extensionImage);
+    if (OverlapNotFound == correctOverlapIndex)
+    {
+        successful = false;
+    }
 
     return successful;
 }
@@ -124,26 +132,76 @@ int ImageCombiner::ConvoluteMatrices(const int p_ObserveMatrix[KernelSize][Kerne
     return sum;
 }
 
+void ImageCombiner::AnalyzeTop(const QImage& p_finalImage, const QImage& p_extensionImage)
+{
+    bool overlapFound = false;
+
+    int ConstantY = ImageBorderPixels;
+    for (int xFinalImageIndex = ImageBorderPixels; xFinalImageIndex < (p_finalImage.width() - ImageBorderPixels - OverlapPixels); xFinalImageIndex += SearchPixelInterval)
+    {
+        overlapFound = FindOverlap(p_finalImage, p_extensionImage, xFinalImageIndex, ConstantY);
+        if (overlapFound)
+        {
+            m_overlapXStartPixelsFinalImage.push_back(xFinalImageIndex);
+            m_overlapYStartPixelsFinalImage.push_back(ConstantY);
+        }
+    }
+}
+
+void ImageCombiner::AnalyzeLeft(const QImage& p_finalImage, const QImage& p_extensionImage)
+{
+    int ConstantX = ImageBorderPixels;
+    for (int yFinalImageIndex = ImageBorderPixels + OverlapPixels; yFinalImageIndex < (p_finalImage.height() - ImageBorderPixels - OverlapPixels); yFinalImageIndex += SearchPixelInterval)
+    {
+        bool overlapFound = FindOverlap(p_finalImage, p_extensionImage, ConstantX, yFinalImageIndex);
+        if (overlapFound)
+        {
+            m_overlapXStartPixelsFinalImage.push_back(ConstantX);
+            m_overlapYStartPixelsFinalImage.push_back(yFinalImageIndex);
+        }
+    }
+}
+
+void ImageCombiner::AnalyzeRight(const QImage& p_finalImage, const QImage& p_extensionImage)
+{
+    int ConstantX = p_finalImage.width() - ImageBorderPixels - OverlapPixels;
+    for (int yFinalImageIndex = ImageBorderPixels + OverlapPixels; yFinalImageIndex < (p_finalImage.height() - ImageBorderPixels - OverlapPixels); yFinalImageIndex += SearchPixelInterval)
+    {
+        bool overlapFound = FindOverlap(p_finalImage, p_extensionImage, ConstantX, yFinalImageIndex);
+        if (overlapFound)
+        {
+            m_overlapXStartPixelsFinalImage.push_back(ConstantX);
+            m_overlapYStartPixelsFinalImage.push_back(yFinalImageIndex);
+        }
+    }
+}
+
+void ImageCombiner::AnalyzeBottom(const QImage& p_finalImage, const QImage& p_extensionImage)
+{
+    int ConstantY = p_finalImage.height() - ImageBorderPixels - OverlapPixels;
+    for (int xFinalImageIndex = ImageBorderPixels; xFinalImageIndex < (p_finalImage.width() - ImageBorderPixels - OverlapPixels); xFinalImageIndex += SearchPixelInterval)
+    {
+       bool overlapFound = FindOverlap(p_finalImage, p_extensionImage, xFinalImageIndex, ConstantY);
+       if (overlapFound)
+       {
+           m_overlapXStartPixelsFinalImage.push_back(xFinalImageIndex);
+           m_overlapYStartPixelsFinalImage.push_back(ConstantY);
+       }
+    }
+}
+
 bool ImageCombiner::FindOverlap(const QImage& p_finalImage, const QImage& p_extensionImage)
 {
     bool overlapFound = false;
 
-    for (int xFinalImageIndex = 1; xFinalImageIndex < (p_finalImage.width() - OverlapPixels); xFinalImageIndex += SearchPixelInterval)
+    AnalyzeTop(p_finalImage, p_extensionImage);
+    AnalyzeLeft(p_finalImage, p_extensionImage);
+    AnalyzeRight(p_finalImage, p_extensionImage);
+    AnalyzeBottom(p_finalImage, p_extensionImage);
+
+    if (m_overlapXStartPixelsFinalImage.size() > 0)
     {
-        for (int yFinalImageIndex = 1; yFinalImageIndex < (p_finalImage.height() - OverlapPixels); yFinalImageIndex += SearchPixelInterval)
-        {
-            overlapFound = FindOverlap(p_finalImage, p_extensionImage, xFinalImageIndex, yFinalImageIndex);
-            if (overlapFound)
-            {
-                m_overlapXStartFinalImage = xFinalImageIndex;
-                m_overlapYStartFinalImage = yFinalImageIndex;
-                break;
-            }
-        }
-        if (overlapFound)
-        {
-            break;
-        }
+        overlapFound = true;
     }
 
     return overlapFound;
@@ -176,12 +234,13 @@ bool ImageCombiner::FindOverlap(const QImage& p_finalImage, const QImage& p_exte
 bool ImageCombiner::FindOverlapPixels(const QImage& p_image, const int p_expectedPixels[OverlapPixels][OverlapPixels])
 {
     bool overlapFound = false;
-    int x = 1;
-    int y = 1;
+    int x = ImageBorderPixels;
+    int y = ImageBorderPixels;
     int overlapCounter = 0;
     int overlapGap = 0;
     int lastSuccessfulOverlapX = 0;
-
+    int MaxImageX = p_image.width() - ImageBorderPixels;
+    int MaxImageY = p_image.height() - ImageBorderPixels;
     do {
         QRgb imagePixelRGB = p_image.pixel(x, y);
         int imagePixelGray = qGray(imagePixelRGB);
@@ -201,36 +260,39 @@ bool ImageCombiner::FindOverlapPixels(const QImage& p_image, const int p_expecte
                     overlapCounter = 0;
                 }
             }
-            if (lastSuccessfulOverlapX > 0)
-            {
-                x = lastSuccessfulOverlapX;
-                lastSuccessfulOverlapX = 0;
-            }
         }
         x++;
 
         if ((overlapCounter > 0) && (0 == (overlapCounter % OverlapPixels)))
         {
-            lastSuccessfulOverlapX = x;
             x -= OverlapPixels;
-            if (x < 0)
-            {
-                x = 0;
-            }
+            lastSuccessfulOverlapX = x;
             y++;
         }
-        if (x >= p_image.width())
+        if (x >= MaxImageX)
         {
+            // Fill overlap counter to number of OverlapPixels in x-direction
+            if (overlapCounter > MinOverlapPixelsAtBorder)
+            {
+                x -= (overlapCounter % OverlapPixels);
+                lastSuccessfulOverlapX = x;
+                overlapCounter += (OverlapPixels - (overlapCounter % OverlapPixels));
+                
+            }
             x = 0;
             y++;
         }
-    } while (overlapCounter < (OverlapPixels * OverlapPixels) && (y < p_image.height()));
+    } while (overlapCounter < (OverlapPixels * OverlapPixels) && (y < MaxImageY));
 
-    if ((OverlapPixels * OverlapPixels) == overlapCounter)
+    if (((OverlapPixels * OverlapPixels) == overlapCounter) || (MaxImageY == y && overlapCounter > MinOverlapPixelsAtBorder))
     {
-        m_overlapXStartExtensionImage = lastSuccessfulOverlapX - OverlapPixels;
-        m_overlapYStartExtensionImage = y - OverlapPixels;
+        m_overlapXStartPixelsExtensionImage.push_back(lastSuccessfulOverlapX);
+        m_overlapYStartPixelsExtensionImage.push_back(y - (overlapCounter / OverlapPixels));
         overlapFound = true;
+    }
+    else
+    {
+        overlapFound = false;
     }
 
     return overlapFound;
@@ -246,4 +308,99 @@ bool ImageCombiner::HasSameGrayValue(int p_firstValue, int p_secondValue)
     }
 
     return hasSameGrayValue;
+}
+
+int ImageCombiner::FindCorrectOneOfOverlapCandidates(const QImage& p_finalImage, const QImage& p_extensionImage)
+{
+    int correctIndex = FindCorrectOneOfOverlapCandidatesX(p_finalImage, p_extensionImage);
+    if (OverlapNotFound != correctIndex)
+    {
+        correctIndex = FindCorrectOneOfOverlapCandidatesY(p_finalImage, p_extensionImage);
+    }
+
+    return correctIndex;
+}
+
+int ImageCombiner::FindCorrectOneOfOverlapCandidatesX(const QImage& p_finalImage, const QImage& p_extensionImage)
+{
+    int correctIndex = OverlapNotFound;
+    bool overlapSuccessful = false;
+
+    for (int i = 0; i < m_overlapXStartPixelsFinalImage.size(); i++)
+    {
+        int currentPixelXFinalImage = m_overlapXStartPixelsFinalImage.at(i) + OverlapPixels;
+        int currentPixelXExtensionImage = m_overlapXStartPixelsExtensionImage.at(i) + OverlapPixels;
+        int currentPixelYFinalImage = m_overlapYStartPixelsFinalImage.at(i);
+        int currentPixelYExtensionImage = m_overlapYStartPixelsExtensionImage.at(i);
+
+        while (currentPixelXFinalImage < (p_finalImage.width() - ImageBorderPixels) && currentPixelXExtensionImage < (p_extensionImage.width() - ImageBorderPixels))
+        {
+            QRgb finalImagePixelRGB = p_finalImage.pixel(currentPixelXFinalImage, currentPixelYFinalImage);
+            int finalImagePixelGray = qGray(finalImagePixelRGB);
+            QRgb extensionImagePixelRGB = p_extensionImage.pixel(currentPixelXExtensionImage, currentPixelYExtensionImage);
+            int extensionImagePixelGray = qGray(extensionImagePixelRGB);
+
+            if (HasSameGrayValue(finalImagePixelGray, extensionImagePixelGray))
+            {
+                overlapSuccessful = true;
+            }
+            else
+            {
+                overlapSuccessful = false;
+                break;
+            }
+            currentPixelXFinalImage++;
+            currentPixelXExtensionImage++;
+        };
+
+        if (overlapSuccessful)
+        {
+            correctIndex = i;
+            break;
+        }
+    }
+
+    return correctIndex;
+}
+
+int ImageCombiner::FindCorrectOneOfOverlapCandidatesY(const QImage& p_finalImage, const QImage& p_extensionImage)
+{
+    int correctIndex = OverlapNotFound;
+    bool overlapSuccessful = false;
+
+    for (int i = 0; i < m_overlapXStartPixelsFinalImage.size(); i++)
+    {
+        int currentPixelXFinalImage = m_overlapXStartPixelsFinalImage.at(i);
+        int currentPixelXExtensionImage = m_overlapXStartPixelsExtensionImage.at(i);
+        int currentPixelYFinalImage = m_overlapYStartPixelsFinalImage.at(i) + OverlapPixels;
+        int currentPixelYExtensionImage = m_overlapYStartPixelsExtensionImage.at(i) + OverlapPixels;
+
+        while (currentPixelYFinalImage < (p_finalImage.height() - ImageBorderPixels) && currentPixelYExtensionImage < (p_extensionImage.height() - ImageBorderPixels))
+        {
+            QRgb finalImagePixelRGB = p_finalImage.pixel(currentPixelXFinalImage, currentPixelYFinalImage);
+            int finalImagePixelGray = qGray(finalImagePixelRGB);
+            QRgb extensionImagePixelRGB = p_extensionImage.pixel(currentPixelXExtensionImage, currentPixelYExtensionImage);
+            int extensionImagePixelGray = qGray(extensionImagePixelRGB);
+
+            if (HasSameGrayValue(finalImagePixelGray, extensionImagePixelGray))
+            {
+                overlapSuccessful = true;
+            }
+            else
+            {
+                overlapSuccessful = false;
+                break;
+            }
+            currentPixelYFinalImage++;
+            currentPixelYExtensionImage++;
+        };
+
+        if (overlapSuccessful)
+        {
+            correctIndex = i;
+            break;
+        }
+    }
+
+    return correctIndex;
 }
--- Images2WavefrontObj/ImageProcessing/ImageCombiner.h
+++ Images2WavefrontObj/ImageProcessing/ImageCombiner.h
@@ -16,10 +16,13 @@ public:
 private:
     static constexpr int KernelNeighbour = 1;
     static constexpr int KernelSize = 3;
-    static constexpr int OverlapPixels = 200;
+    static constexpr int OverlapPixels = 150;
+    static constexpr int MinOverlapPixelsAtBorder = 50;
     static constexpr int SearchPixelInterval = 50;
-    static constexpr int AllowedOverlapGap = 2;
+    static constexpr int AllowedOverlapGap = 10;
     static constexpr int AllowedGrayScaleDifference = 10;
+    static constexpr int ImageBorderPixels = 1;
+    static constexpr int OverlapNotFound = -1;
 
     static constexpr int SobelKernelMatrixX[KernelSize][KernelSize] = { {1,0,-1},{2,0,-2},{1,0,-1 } };
     static constexpr int SobelKernelMatrixY[KernelSize][KernelSize] = { {1,2,1},{0,0,0},{-1,-2,-1} };
@@ -28,15 +31,27 @@ private:
     int m_overlapYStartFinalImage{ 0 };
     int m_overlapXStartExtensionImage{ 0 };
     int m_overlapYStartExtensionImage{ 0 };
+    std::vector<int> m_overlapXStartPixelsFinalImage{};
+    std::vector<int> m_overlapYStartPixelsFinalImage{};
+    std::vector<int> m_overlapXStartPixelsExtensionImage{};
+    std::vector<int> m_overlapYStartPixelsExtensionImage{};
 
     bool CombineTwoImages(QImage& p_finalImage, const QImage& p_extensionImage);
     void GetGradientImage(const QImage& p_Image, QImage& p_gradientImage);
     void UpdateGradientImage(QImage& p_gradientImage, int p_ObserveMatrix[KernelSize][KernelSize], int p_ImageX, int p_ImageY);
     int ConvoluteMatrices(const int p_ObserveMatrix[KernelSize][KernelSize], const int p_KernelMatrix[KernelSize][KernelSize]);
 
+    void AnalyzeTop(const QImage& p_finalImage, const QImage& p_extensionImage);
+    void AnalyzeLeft(const QImage& p_finalImage, const QImage& p_extensionImage);
+    void AnalyzeRight(const QImage& p_finalImage, const QImage& p_extensionImage);
+    void AnalyzeBottom(const QImage& p_finalImage, const QImage& p_extensionImage);
     bool FindOverlap(const QImage& p_finalImage, const QImage& p_extensionImage);
     bool FindOverlap(const QImage& p_finalImage, const QImage& p_extensionImage, int p_startX, int p_startY);
     bool FindOverlapPixels(const QImage& p_image, const int p_expectedPixels[OverlapPixels][OverlapPixels]);
     bool HasSameGrayValue(int p_firstValue, int p_secondValue);
+
+    int FindCorrectOneOfOverlapCandidates(const QImage& p_finalImage, const QImage& p_extensionImage);
+    int FindCorrectOneOfOverlapCandidatesX(const QImage& p_finalImage, const QImage& p_extensionImage);
+    int FindCorrectOneOfOverlapCandidatesY(const QImage& p_finalImage, const QImage& p_extensionImage);
 };
 
